850436b27b68d1247790a179b7293456adaf9916
Biko-Higiro <bikondabwunze@gmail.com>
2025-03-11 13:54:02 -0400

    started navbar and different pages

---
+++ View/SY-View/src/NavBar.tsx
@@ -0,0 +1,20 @@
+function NavBar() {
+  return (
+    <nav>
+      <a href="/">SAVE YOURSELF</a>
+      <ul>
+        <li>
+          <a href="/About"> About</a>
+        </li>
+        <li>
+          <a href="/Pricing"> Pricing</a>
+        </li>
+        <li>
+          <a href="/Login">Login</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar;
