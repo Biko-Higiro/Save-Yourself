ccd32dc5510882fad6b57dd7ff2574a3e7213830
Biko-Higiro <bikondabwunze@gmail.com>
2025-06-09 18:50:06 -0400

    started the pricing page skeleton

--- View/SY-View/src/Pricing.tsx
+++ View/SY-View/src/Pricing.tsx
@@ -1,5 +1,18 @@
 function Pricing() {
-  return <h1>Pricing</h1>;
+  return (
+    <div className="container Pricing-Container">
+      <div className="Pricing-Header">
+        <h1>Pricing</h1>;
+      </div>
+      <div className="container Prices-Container">
+        <div className="Prices-Items">
+          <p>hi</p>
+        </div>
+        <div className="Prices-Items"></div>
+        <div className="Prices-Items"></div>
+      </div>
+    </div>
+  );
 }
 
 export default Pricing;
