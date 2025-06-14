f817d6128b77d4eb1fe0b8cadd1524a709bf308c
Biko-Higiro <bikondabwunze@gmail.com>
2025-06-14 18:37:36 -0400

    Finished the skeleton of the pricing page

--- View/SY-View/src/Pricing.tsx
+++ View/SY-View/src/Pricing.tsx
@@ -6,10 +6,30 @@ function Pricing() {
       </div>
       <div className="container Prices-Container">
         <div className="Prices-Items">
-          <p>hi</p>
+          <h3 className="Pricing-Items-Header">Free</h3>
+          <p className="Pricing-Items-Text">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus
+            dolore, architecto dolor itaque soluta consequatur consectetur at
+            veritatis commodi deserunt quae exercitationem?
+          </p>
+          <button className="Pricing-Buttons btn btn-primary">Sign Up</button>
         </div>
-        <div className="Prices-Items"></div>
-        <div className="Prices-Items"></div>
+        <div className="Prices-Items">
+          <h3 className="Pricing-Items-Header">Standard</h3>
+          <p className="Pricing-Items-Text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.at veritatis
+            commodi deserunt quae exercitationem?
+          </p>
+          <button className="Pricing-Buttons btn btn-primary">Buy Now</button>
+        </div>
+        <div className="Prices-Items">
+          <h3 className="Pricing-Items-Header"> Premium</h3>
+          <p className="Pricing-Items-Text">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe
+            corporis maiores magnam ab est similique sunt. Eaque, ad placeat.
+          </p>
+          <button className="Pricing-Buttons btn btn-primary">Upgrade</button>
+        </div>
       </div>
     </div>
   );
